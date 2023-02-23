868939d00d93510769f291a9b2dbfa005fcc8f8b
anushaa15 <anushasaini2002@gmail.com>
2023-02-23 10:12:41 +0530

    first commit

---
+++ app/src/main/java/com/example/swaadish/homepage_01.java
@@ -0,0 +1,24 @@
+package com.example.swaadish;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homepage_01 extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage01);
+
+
+    }
+}
