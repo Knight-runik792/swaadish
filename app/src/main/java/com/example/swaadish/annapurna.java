c8ae1563a815b6c8bec06fa2fcca6833ed256904
anushaa15 <anushasaini2002@gmail.com>
2023-02-23 18:53:01 +0530

    first command

---
+++ app/src/main/java/com/example/swaadish/annapurna.java
@@ -0,0 +1,14 @@
+package com.example.swaadish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class annapurna extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_annapurna);
+    }
+}
